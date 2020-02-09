8161aa1e5ba3c9495932a8db34a1ca8f55dbaca4
Juan M.vi <earth001@gmail.com>
2020-02-08 23:32:08 -0300

    UPDATE MRJAR Sample

--- people/src/main/java/module-info.java
+++ people/src/main/java/module-info.java
@@ -2,5 +2,6 @@ module people {
   exports com.name;
   requires transitive header;
   requires com.fasterxml.jackson.databind;
+  requires java.logging;
   opens com.name to com.fasterxml.jackson.databind;
 }
--- people/src/main/java12/com/name/NameFactory.java
+++ people/src/main/java12/com/name/NameFactory.java
@@ -2,14 +2,16 @@ package com.name;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class NameFactory implements Factory {
+
+  private static final Logger log = Logger.getLogger(NameFactory.class.getName());
   private final String datasource;
 
   public NameFactory(String datasource) {
@@ -17,9 +19,10 @@ public class NameFactory implements Factory {
   }
 
   public List<Name> getNames() throws IOException, URISyntaxException {
-    System.out.println("getNames from Java 10");
+    log.info("getNames from Java 12".indent(4));
     var input = new String(
-        Files.readAllBytes(Paths.get(ClassLoader.getSystemResource(datasource).toURI())),
+        getClass().getResourceAsStream(
+            String.format("%c%s", File.separatorChar, datasource)).readAllBytes(),
         StandardCharsets.UTF_8);
     var mapper = new ObjectMapper();
     return mapper.readValue(input, new TypeReference<>() {
