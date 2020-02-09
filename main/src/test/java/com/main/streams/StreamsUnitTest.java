0ad689fd0a0f750269106ee80fb5ad7adaed7d0f
Juan M.vi <earth001@gmail.com>
2020-02-09 15:11:37 -0300

    FEAT Added samples with Optionals

--- main/src/test/java/com/main/streams/StreamsUnitTest.java
+++ main/src/test/java/com/main/streams/StreamsUnitTest.java
@@ -1,6 +1,10 @@
 package com.main.streams;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.assertj.core.api.SoftAssertions;
@@ -47,7 +51,6 @@ class StreamsUnitTest {
 
   @Test
   void whenIterateStream_thenExpectedList() {
-
     var accounts = Stream
         .iterate(new SavingsAccount(100), account -> account.getBalance() <= 500,
             account -> new SavingsAccount(account.getBalance() + 100)).collect(Collectors.toList());
@@ -60,4 +63,48 @@ class StreamsUnitTest {
     });
   }
 
+  @Test
+  void whenFilterListByLimit_thenGetFilteredAmount() {
+    int limit = 400;
+    var accounts = List
+        .of(new SavingsAccount(100), new SavingsAccount(200), new SavingsAccount(300),
+            new SavingsAccount(400), new SavingsAccount(500));
+
+    double amountFiltered = accounts.stream().filter(account -> account.getBalance() >= limit)
+        .findAny().stream().mapToDouble(SavingsAccount::getBalance).sum();
+
+    assertThat(amountFiltered).isGreaterThanOrEqualTo(limit);
+  }
+
+  @Test
+  void whenFilterListByUpperLimit_thenDefaultAccount() {
+    int limit = 600;
+    var defaultAmount = Optional.of(new SavingsAccount(0));
+    var accounts = List
+        .of(new SavingsAccount(100), new SavingsAccount(200), new SavingsAccount(300),
+            new SavingsAccount(400), new SavingsAccount(500));
+
+    double amountFiltered = accounts.stream().filter(account -> account.getBalance() >= limit)
+        .findAny().or(() -> defaultAmount).stream().mapToDouble(SavingsAccount::getBalance).sum();
+
+    assertThat(amountFiltered).isEqualTo(0);
+  }
+
+  @Test
+  void whenFilterListByUpperLimitWithOptionals_thenDefaultBalance()
+      throws IllegalArgumentException {
+    int limit = 600;
+    double defaultBalance = 0;
+    var defaultAccount = new SavingsAccount(defaultBalance);
+    var accounts = List
+        .of(new SavingsAccount(100), new SavingsAccount(200), new SavingsAccount(300),
+            new SavingsAccount(400), new SavingsAccount(500));
+    var result = new ArrayList<SavingsAccount>();
+
+    accounts.stream().filter(account -> account.getBalance() >= limit)
+        .findAny().ifPresentOrElse(account -> result.add(account), () ->
+        result.add(defaultAccount));
+
+    assertThat(result).hasSize(1).contains(defaultAccount);
+  }
 }
